e02558b070e8e4dfd40ef4264b589b18c18a457e
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2022-12-16 09:35:14 +0200

    use effect

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,9 +1,9 @@
-import { useState, Component } from 'react';
+import { useState, useEffect, Component } from 'react';
 import history from '../history';
 import routes from '../routes';
 
 
-export default class Router extends Component {
+/*export default class Router extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -23,16 +23,21 @@ export default class Router extends Component {
   render() {
     return this.state.Component ? this.state.Component : <div>Not component is set</div>;
   }
-}
-/*
+}*/
+
+
 export default function({ href }) {
   const [Component, setComponent] = useState(null)
   const handle = async ({location}) => {
     console.log('locations is', location);
-    const route = await routes.resolve(location)
+    const route = await routes.resolve(location.pathname)
     //const component = await import(`./riot/pages/${route.page}.riot`)
-    //setComponent(route.page);
+    setComponent(route.page);
     console.log('component is', Component)
   }
-  history.listen(handle)
-  */
+  useEffect(() => {
+    const clear = history.listen(handle);
+    return clear;
+  });
+  return Component ? Component : <div>Not component is set</div>;
+}
